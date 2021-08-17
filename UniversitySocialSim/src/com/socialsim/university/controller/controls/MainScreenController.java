b91a6e4508d65d8c759b00d6add200775502e223
gabminamedez <gabminamedez@gmail.com>
2021-08-17 10:31:31 +0800

    restructuring of files

---
+++ UniversitySocialSim/src/com/socialsim/university/controller/controls/MainScreenController.java
@@ -0,0 +1,7 @@
+package com.socialsim.university.controller.controls;
+
+import com.socialsim.university.controller.Controller;
+
+public class MainScreenController extends Controller {
+
+}
--- UniversitySocialSim/src/com/socialsim/university/controller/controls/screens/MainScreenController.java
+++
@@ -1,7 +0,0 @@
-package com.socialsim.university.controller.controls.screens;
-
-import com.socialsim.university.controller.controls.ScreenController;
-
-public class MainScreenController extends ScreenController {
-
-}
