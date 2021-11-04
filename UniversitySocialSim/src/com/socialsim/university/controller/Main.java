e19c40538e1f6b4d33615b83d2ebb8cf0cf1910a
Gabriel Minamedez <gabminamedez@gmail.com>
2021-11-04 16:31:01 +0800

    [U] Experimenting with controller if functioning

--- UniversitySocialSim/src/com/socialsim/university/controller/Main.java
+++ UniversitySocialSim/src/com/socialsim/university/controller/Main.java
@@ -1,5 +1,6 @@
 package com.socialsim.university.controller;
 
+import com.socialsim.university.controller.controls.MainScreenController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -7,6 +8,8 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
+    public static MainScreenController mainScreenController;
 
     public static void main(String[] args) {
         launch(args);
@@ -16,6 +19,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/com/socialsim/university/view/MainScreen.fxml"));
         primaryStage.setScene(new Scene(root));
+        primaryStage.setTitle("University SocialSim");
         primaryStage.show();
     }
 
