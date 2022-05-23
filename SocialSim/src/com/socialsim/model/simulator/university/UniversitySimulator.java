7e612bb8a8049aa72b9f24a0ebfad4066a35ab96
Gabriel Minamedez <gabminamedez@gmail.com>
2022-05-23 19:23:36 +0800

    Experiments for University COVID 4

--- SocialSim/src/com/socialsim/model/simulator/university/UniversitySimulator.java
+++ SocialSim/src/com/socialsim/model/simulator/university/UniversitySimulator.java
@@ -522,21 +522,19 @@ public class UniversitySimulator extends Simulator {
                         }
                     }
                     // Logic for Bathroom Wait
-//                    if (agent.getAgentMovement().getCurrentState().getName() == UniversityState.Name.WAIT_INFRONT_OF_CLASS && agent.getAgentMovement().getCurrentState().getClassroomID() == 6){
-//                        if (!university.allBathroomsOccupied()){
-//                            if (!agent.getAgentMovement().chooseBathroomGoal(Toilet.class)) {
-//                                agent.getAgentMovement().getRoutePlan().getCurrentRoutePlan().add(agent.getAgentMovement().getStateIndex() - 1, agent.getAgentMovement().getRoutePlan().addWaitingRoute(6,0,agent));
-//                                agent.getAgentMovement().setPreviousState(agent.getAgentMovement().getStateIndex());
-//                                agent.getAgentMovement().setStateIndex(agent.getAgentMovement().getStateIndex() -1);
-//                                agent.getAgentMovement().setActionIndex(0);
-//                                agent.getAgentMovement().setCurrentAction(agent.getAgentMovement().getCurrentState().getActions().get(agent.getAgentMovement().getActionIndex()));
-//                                if(agent.getAgentMovement().getGoalAttractor() != null) {
-//                                    agent.getAgentMovement().getGoalAttractor().setIsReserved(false);
-//                                }
-//                                agent.getAgentMovement().resetGoal();
-//                            }
-//                        }
-//                    }
+                    if (agent.getAgentMovement().getCurrentState().getName() == UniversityState.Name.WAIT_INFRONT_OF_CLASS && agent.getAgentMovement().getCurrentState().getClassroomID() == 6){
+                        if (bathroomReserves > 0){
+                            agent.getAgentMovement().setNextState(agent.getAgentMovement().getStateIndex());
+                            agent.getAgentMovement().setStateIndex(agent.getAgentMovement().getStateIndex() + 1);
+                            agent.getAgentMovement().setActionIndex(0);
+                            agent.getAgentMovement().setCurrentAction(agent.getAgentMovement().getCurrentState().getActions().get(agent.getAgentMovement().getActionIndex()));
+                            if (agent.getAgentMovement().getGoalAttractor() != null) {
+                                agent.getAgentMovement().getGoalAttractor().setIsReserved(false);
+                            }
+                            agent.getAgentMovement().resetGoal();
+                            bathroomReserves--;
+                        }
+                    }
                 }
                 moveOne(agent);
             } catch (Throwable ex) {
